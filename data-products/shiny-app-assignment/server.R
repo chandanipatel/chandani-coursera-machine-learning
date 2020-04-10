586ebd5fd481c24442b40db18d54bb4029656700
Chandani Patel <chpatel@arlo.com>
2020-04-10 11:14:33 +0530

    Updated app

--- data-products/shiny-app-assignment/server.R
+++ data-products/shiny-app-assignment/server.R
@@ -23,10 +23,25 @@ server <- function(input, output) {
     })
     
     histogram_Sex <- reactive({
-        sexFilter <- input$sex
+        sexFilter <- as.numeric(input$sex)
     })
     histogram_Age <- reactive({
         values <- c(min(input$age), max(input$age))
+    })
+    
+    dataGenderwise <- reactive({
+        ageFilter = histogram_Age()
+        min=ageFilter[1]
+        max=ageFilter[2]
+        
+        filter_data = heart_diease_uci[heart_diease_uci$age>min, ]
+        filter_data = filter_data[filter_data$age<max, ]
+        sexFilter = histogram_Sex()
+        if(sexFilter != 3) {
+            filter_data = filter_data[filter_data$sex == sexFilter,]
+        }
+        sex=filter_data$sex
+        sex_wise_data=data.frame(table(unlist(sex)))
     })
     
     output$sexwisePlot <- renderPlot({
@@ -39,7 +54,7 @@ server <- function(input, output) {
         filter_data = heart_diease_uci[heart_diease_uci$age>min, ]
         filter_data = filter_data[filter_data$age<max, ]
         
-        if(sexFilter == 3) {
+        if(sexFilter == "3") {
             age=filter_data$age
         } else {
             age=filter_data[filter_data$sex == sexFilter,]['age']
@@ -59,13 +74,15 @@ server <- function(input, output) {
         
         filter_data = heart_diease_uci[heart_diease_uci$age>min, ]
         filter_data = filter_data[filter_data$age<max, ]
+        sexFilter = histogram_Sex()
+        if(sexFilter != 3) {
+            filter_data = filter_data[filter_data$sex == sexFilter,]
+        }
         sex=filter_data$sex
         sex_wise_data=data.frame(table(unlist(sex)))
         colnames(sex_wise_data)[1]<-'sex'
-        print(sex_wise_data)
         sex_wise_data$sex <- with(sex_wise_data, ifelse(sex==1, 'Male', "Female"))
         
-        print(sex_wise_data)
         ggplot(sex_wise_data, aes(x="", y=Freq, fill=sex)) +
             geom_bar(stat="identity", width=1) +
             coord_polar("y", start=0) +
@@ -79,6 +96,12 @@ server <- function(input, output) {
         
         filter_data = heart_diease_uci[heart_diease_uci$age>min, ]
         filter_data = filter_data[filter_data$age<max, ]
+        
+        sexFilter = histogram_Sex()
+        if(sexFilter != 3) {
+            filter_data = filter_data[filter_data$sex == sexFilter,]
+        }
+        
         cp=filter_data$cp
         cp_wise_data=data.frame(table(unlist(cp)))
         colnames(cp_wise_data)[1]<-'cp'
@@ -89,5 +112,38 @@ server <- function(input, output) {
             scale_fill_manual(values=c("#EF746C", "#4DBB37", "#F4C050", "#327791"))
     })
     
+    output$maleStatestics <- renderValueBox({
+        sex_wise_data = dataGenderwise()
+        male = sex_wise_data$Freq[1]
+        total = sum(sex_wise_data$Freq)
+        malepercent = percent(male/total)
+                valueBox(
+            "Male", paste0("Total: ", male, " Percentage: ", malepercent), icon = icon("male"),
+            color = "orange"
+        )
+    })
     
+    output$femaleStatestics <- renderValueBox({
+        sex_wise_data = dataGenderwise()
+        print(sex_wise_data)
+        total = sum(sex_wise_data$Freq)
+        female = sex_wise_data$Freq[2]
+        femalepercent = percent(female/total)
+        
+        valueBox(
+            "Female", paste0("Total: ", female, " Percentage: ", femalepercent), icon = icon("female"),
+            color = "olive"
+        )
+    })
+    
+    output$totalStatestics <- renderValueBox({
+        sex_wise_data = dataGenderwise()
+        
+        total = sum(sex_wise_data['Freq'])
+        text
+        valueBox(
+            "Total", paste0("Total: ", total), icon = icon("list"),
+            color="blue"
+        )
+    })
 }
--- data-products/shiny-app-assignment/ui.R
+++ data-products/shiny-app-assignment/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(shinydashboard)
 
@@ -22,49 +13,32 @@ ui <- dashboardPage(
         )
     ),
     dashboardBody(
+        column(width = 12,
+               valueBoxOutput("totalStatestics"),
+               valueBoxOutput("maleStatestics"),
+               valueBoxOutput("femaleStatestics")
+        ),
         # Sex wise histogram
         column(width = 12,
            box(
                title = "Histogram Based on Age & Sex", width = NULL, solidHeader = TRUE, status = "primary",
-                plotOutput("sexwisePlot", height = 350,
-                  click = "plot_click",
-                  dblclick = dblclickOpts(
-                      id = "plot_dblclick"
-                  ),
-                  hover = hoverOpts(
-                      id = "plot_hover"
-                  ),
-                  brush = brushOpts(
-                      id = "plot_brush"
-                  )
-                )
+                plotOutput("sexwisePlot", height = 300)
            ),
         ),
-        
         # Other wise histogram
         column(width = 6,
                box(
                    title = "Count based on Gender", width = NULL, solidHeader = TRUE, status = "primary",
-                   plotOutput("genderPlot", height = 200,
-                              click = "plot_click",
-                              dblclick = dblclickOpts(
-                                  id = "plot_dblclick"
-                              ),
-                              hover = hoverOpts(
-                                  id = "plot_hover"
-                              ),
-                              brush = brushOpts(
-                                  id = "plot_brush"
-                              )
-                   )
+                   plotOutput("genderPlot", height = 200)
                ),
         ),
         # Other wise histogram
         column(width = 6,
-               box(
-                   title = "Chest Pain Type", width = NULL, solidHeader = TRUE, status = "primary",
-                   plotOutput("chestpainPlot", height = 200)
-               ),
+                   
+                   box(
+                       title = "Chest Pain Type", width = NULL, solidHeader = TRUE, status = "primary",
+                       plotOutput("chestpainPlot", height = 200)
+                   )
         )
     )
 )
